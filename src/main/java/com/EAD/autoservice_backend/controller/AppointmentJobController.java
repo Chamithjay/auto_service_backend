7799a683567112d272889565599f6eee4088cb1e
Kalani09 <kalanichandrasena@gmail.com>
2025-11-03 23:57:46 +0530

    Updated appointment, service-item, and vehicle controller

--- src/main/java/com/EAD/autoservice_backend/controller/AppointmentJobController.java
+++ src/main/java/com/EAD/autoservice_backend/controller/AppointmentJobController.java
@@ -1,13 +1,14 @@
 package com.EAD.autoservice_backend.controller;
 
-import com.EAD.autoservice_backend.dto.AppointmentJobResponse;
+import com.EAD.autoservice_backend.dto.EmployeeAppointmentJobResponse;
+import com.EAD.autoservice_backend.dto.EmployeeAppointmentJobStatusUpdateRequest;
+import com.EAD.autoservice_backend.dto.EmployeeJobNoteRequest;
 import com.EAD.autoservice_backend.service.AppointmentJobService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/appointment-jobs")
@@ -21,15 +22,42 @@ public class AppointmentJobController {
 
 
     // Get appointment job information by id.
-    @RequestMapping("/{id}")
-    public ResponseEntity<AppointmentJobResponse> getAppointmentJobById(@PathVariable("id") Long appointmentJobId){
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeAppointmentJobResponse> getAppointmentJobById(@PathVariable("id") Long appointmentJobId){
         try{
-
-            AppointmentJobResponse appointmentJobResponse = appointmentJobService.getAppointmentById(appointmentJobId);
-            return ResponseEntity.ok(appointmentJobResponse);
+            EmployeeAppointmentJobResponse employeeAppointmentJobResponse = appointmentJobService.getAppointmentById(appointmentJobId);
+            return new ResponseEntity<>(employeeAppointmentJobResponse, HttpStatus.OK);
 
         }catch (EntityNotFoundException e){
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Save job note for an appointment job.
+    @PatchMapping("/save-job-note/{id}")
+    public ResponseEntity<EmployeeAppointmentJobResponse> saveJobNoteForAppointmentJob(@PathVariable("id") Long appointmentJobId, @Valid @RequestBody EmployeeJobNoteRequest employeeJobNoteRequest){
+        try {
+            EmployeeAppointmentJobResponse employeeAppointmentJobResponse = appointmentJobService.saveJobNoteForAppointmentJob(appointmentJobId, employeeJobNoteRequest);
+            return new ResponseEntity<>(employeeAppointmentJobResponse, HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Update job status for an appointment job.
+    @PatchMapping("/update-job-status/{id}")
+    public ResponseEntity<String> updateJobStatusForAppointmentJob(@PathVariable("id") Long appointmentJobId, @Valid @RequestBody EmployeeAppointmentJobStatusUpdateRequest statusUpdateRequest){
+        try {
+            String response = appointmentJobService.updateAppointmentJobStatus(appointmentJobId, statusUpdateRequest.getJobStatus());
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/com/EAD/autoservice_backend/controller/ServiceItemController.java
+++ src/main/java/com/EAD/autoservice_backend/controller/ServiceItemController.java
@@ -1,9 +1,10 @@
 package com.EAD.autoservice_backend.controller;
 
-import com.EAD.autoservice_backend.dto.ServiceItemResponse;
+import com.EAD.autoservice_backend.dto.EmployeeServiceItemResponse;
 import com.EAD.autoservice_backend.service.ServiceItemService;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,21 +17,20 @@ public class ServiceItemController {
 
     private final ServiceItemService serviceItemService;
 
-    @Autowired
     public ServiceItemController(ServiceItemService serviceItemService) {
         this.serviceItemService = serviceItemService;
     }
 
     // Get service item information by id.
     @GetMapping("/{id}")
-    public ResponseEntity<ServiceItemResponse> getServiceItemById(@PathVariable("id") Long serviceItemId) {
-        try{
-            System.out.println("hello" + serviceItemId);
-            ServiceItemResponse serviceItemResponse = serviceItemService.getServiceItemById(serviceItemId);
-            return ResponseEntity.ok(serviceItemResponse);
-
-        }catch (EntityNotFoundException e){
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<EmployeeServiceItemResponse> getServiceItemById(@PathVariable("id") Long serviceItemId) {
+        try {
+            EmployeeServiceItemResponse employeeServiceItemResponse = serviceItemService.getServiceItemById(serviceItemId);
+            return new ResponseEntity<>(employeeServiceItemResponse, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
