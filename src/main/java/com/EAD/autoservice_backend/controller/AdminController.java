66136442da804f7f2f53808344b705f5d247abc0
Chamitha Jayavihan Thambiliyagoda <124007079+Chamithjay@users.noreply.github.com>
2025-11-05 22:07:38 +0530

    Error resolved after merge heshani

--- src/main/java/com/EAD/autoservice_backend/controller/AdminController.java
+++ src/main/java/com/EAD/autoservice_backend/controller/AdminController.java
@@ -17,7 +17,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api/v1/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
